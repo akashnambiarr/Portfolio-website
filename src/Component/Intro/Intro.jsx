ab2d58e0c3e050432974177aca62a9d85026ee23
Akash Nambiar <akash.n@holosuit.com>
2021-06-13 00:26:41 +0530

    done with intro page

--- src/Component/Intro/Intro.jsx
+++ src/Component/Intro/Intro.jsx
@@ -1,12 +1,34 @@
 import './Intro.scss'
+import { init } from 'ityped'
+import { useEffect, useRef} from 'react';
 
 export default function Intro() {
+    const textRef = useRef();
+
+    useEffect(() => {
+        init(textRef.current, {
+            backDelay:1500,
+            backSpeed: 60,
+            showCursor: true,
+            strings: ["Developer", "Creator","Product Enthusiast"],
+        });
+    },[])
     return (
         <div className = 'intro' id = "intro">
             <div className="left">
                 <div className="imageContainer">
                     <img src="assets/mypic.png" alt="" />
                 </div>
+            </div>
+            <div className="right">
+                <div className="wrapper">
+                    <h2>Hi There, I'm</h2>
+                    <h1>Akash Nambiar</h1>
+                    <h3>Freelance <span ref = {textRef}></span></h3>
+                </div>
+                <a href = "#portfolio">
+                    <img src = "assets/down-arrow.png" alt =''/>
+                </a>
             </div>
 
             <div className="right"></div>
--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -3,7 +3,20 @@ import './Portfolio.scss'
 export default function Portfolio() {
     return (
         <div className = "portfolio" id = "portfolio" >
-            
+            <h1>Portfolio</h1>
+            <ul>
+                <li>Featured</li>
+                <li>Web App</li>
+                <li>Mobile App</li>
+                <li>Design</li>
+                <li>Branding</li>
+            </ul>
+            <div className="container">
+                <div className="item">
+                    <img src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+            </div>
         </div>
     )
 }
