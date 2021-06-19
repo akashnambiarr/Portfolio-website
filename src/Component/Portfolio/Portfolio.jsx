4bcad138e8829b18f7130f0200b142d90a2c87e3
Akash Nambiar <akash.n@holosuit.com>
2021-06-20 01:44:11 +0530

    fixed the portfolio page issue

--- src/Component/Portfolio/Portfolio.jsx
+++ src/Component/Portfolio/Portfolio.jsx
@@ -1,22 +1,79 @@
 import './Portfolio.scss'
+import { useState } from 'react';
+import PortfolioList from '../portfolioList/PortfolioList';
 
 export default function Portfolio() {
+
+    const [selected, setSelected]  = useState("feature")
+
+    const list = [
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        {
+            id: "web",
+            title: "Web App",
+        },
+        {
+            id: "mobile",
+            title: "Mobile App",
+        },
+        {
+            id: "design",
+            title: "Design",
+        },
+        {
+            id: "content",
+            title: "Content",
+        },
+        {
+            id: "featured",
+            title: "Featured",
+        },
+        
+    ];
+
     return (
         <div className = "portfolio" id = "portfolio" >
             <h1>Portfolio</h1>
             <ul>
-                <li>Featured</li>
-                <li>Web App</li>
-                <li>Mobile App</li>
-                <li>Design</li>
-                <li>Branding</li>
+               {list.map((item) => (
+                   <PortfolioList title = {item.title} active = 
+                   {selected === item.id} setSelected = {setSelected}/>
+               ))}
             </ul>
             <div className="container">
                 <div className="item">
-                    <img src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
+                    <h3>Banking App</h3>
+                </div>
+                <div className="item">
+                    <img className = "photo" src="https://www.mindinventory.com/blog/wp-content/uploads/2018/10/banking-app-1.png" alt="" />
                     <h3>Banking App</h3>
                 </div>
             </div>
+            
         </div>
     )
 }
---
+++ src/Component/portfolioList/PortfolioList.jsx
@@ -0,0 +1,9 @@
+import "./portfolioList.scss"
+
+export default function PortfolioList({title,active,setSelected}) {
+    return (
+        <li className = {active ? "portfolioList active" : "portfolioList"}>
+            {title}
+        </li>
+    )
+}
